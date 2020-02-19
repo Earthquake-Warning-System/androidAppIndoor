ed084295bc02d96eea858be0632fffabc846976f
呂健豪 (106503522) <ncu106503522@office365.ncu.edu.tw>
2020-02-19 17:05:39 +0800

    remove kpalive random for test, add store eq time in sharedpreferences

--- app/src/main/java/com/example/udptest/KpAlive.kt
+++ app/src/main/java/com/example/udptest/KpAlive.kt
@@ -5,7 +5,7 @@ import java.util.*
 import kotlin.concurrent.timerTask
 
 class KpAlive {
-    fun randomtime (time : Long){
+    /*fun randomtime (time : Long){
         Timer().schedule(timerTask {
             if(MainActivity.serverIp != "" || MainActivity.serverPort != 0) {
                 //kpalive with server
@@ -18,5 +18,17 @@ class KpAlive {
                 randomtime( (1800000..5400000).random().toLong())
             }
         },time)
+    }*/
+    fun randomtime (time : Long){
+        Timer().schedule(timerTask {
+            if(MainActivity.serverIp != "" || MainActivity.serverPort != 0) {
+                //kpalive with server
+                UdpSender(MainActivity.socket).kpAliveSend()
+                Log.d("kp time",time.toString())
+            }else{
+                UdpSender(MainActivity.socket).bootAskSend()
+                Log.d("ask bs for cs time",time.toString())
+            }
+        },time, 3600000)
     }
 }
